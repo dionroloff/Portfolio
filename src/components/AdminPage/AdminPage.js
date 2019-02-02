2c514e50c7c721065e9c67dd9faaddba416b52ed
Dion Roloff <dion.roloff@gmail.com>
2019-02-02 11:51:22 -0600

    handleClick and handleChange functions added to admin component

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class AdminPage extends Component {
+
+    state = {
+        name: '',
+        description: '',
+        thumbnail: '',
+        website: '',
+        date_completed: '',
+        technology: '',
+    };
+
+
+    handleChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            name: event.target.value,
+            description: event.target.value,
+            thumbnail: event.target.value,
+            website: event.target.value,
+            date_completed: event.target.value,
+            technology: event.target.value,
+        })
+    }
+
+    handleClick = (event) => {
+        event.preventDefault();
+        const action = { type: 'ADD_PROJECT', payload: this.state};
+        this.props.dispatch(action);
+    }
+
+
     render() {
         return (
             <div>
                 <form>
-                    <label>Add a new project</label><br/>
-                    <input type='text' placeholder="name" /><br/>
-                    <input type='text' placeholder="description" /><br/>
-                    <input type='text' placeholder="thumbnail source" /><br/>
-                    <input type='text' placeholder="website" /><br/>
-                    <input placeholder="date completed (dd-mm-yyyy)" /><br/>
-                    <select>
+                    <label>Add a new project</label><br />
+                    <input type='text' placeholder="name" onChange={this.handleChange} /><br />
+                    <input type='text' placeholder="description" onChange={this.handleChange} /><br />
+                    <input type='text' placeholder="thumbnail source" onChange={this.handleChange} /><br />
+                    <input type='text' placeholder="website" onChange={this.handleChange} /><br />
+                    <input placeholder="date completed (dd-mm-yyyy)" onChange={this.handleChange} /><br />
+                    <select onChange={this.handleChange}>
                         <option value="React">React</option>
                         <option value="jQuery">jQuery</option>
                         <option value="Node">Node</option>
@@ -20,6 +50,7 @@ class AdminPage extends Component {
                         <option value="Redux">Redux</option>
                         <option value="HTML">HTML</option>
                     </select>
+                    <input type='submit' value='submit project' onClick={this.handleClick} />
                 </form>
             </div>
         )
