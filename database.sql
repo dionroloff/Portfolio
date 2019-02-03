7d6069c3bcc1250d3c0969784d44b55e5c190bb4
Dion Roloff <dion.roloff@gmail.com>
2019-02-03 13:32:46 -0600

    added databse.sql

---
+++ database.sql
@@ -0,0 +1,4 @@
+--An example project
+
+INSERT INTO "projects" ("name", "description", "thumbnail", "website", "date_completed", "tag_id")
+VALUES ('another project', 'its awesome', 'pic.png', 'awesome.com', '04-03-9009', 5);
